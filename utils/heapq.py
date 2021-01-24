cdae1dbdcbd6f3b0418af678d1b911791b232550
Antonio Sapuppo <antoniosapuppo@yahoo.it>
2021-01-24 08:55:43 +0100

    initial fork

--- utils/heapq.py
+++ utils/heapq.py
@@ -2,37 +2,66 @@
 # priority queues on elastic search apache curator
 # sites:
 # https://www.elastic.co/guide/en/elasticsearch/reference/current/docker.html
-# https://curator.readthedocs.io/en/latest/index.html
+# https://elasticsearch-py.readthedocs.io/en/7.10.0/index.html
+# https://elasticsearch-dsl.readthedocs.io
+# https://coralogix.com/log-analytics-blog/42-elasticsearch-query-examples-hands-on-tutorial/
 #
 import elasticsearch as es
-import curator
+from elasticsearch_dsl import Search
+import pickle
+import base64
 
 
 class HeapElastic:
     def __init__(self):
         self.name = 'priority_queue'
         self.cli = es.Elasticsearch(host='localhost', port=9200)
-        self.co = curator.CreateIndex(cli, self.name, extra_settings={
-            "settings": {
-                "number_of_shards": 1,
-                "number_of_replicas": 0
-            },
-            "mappings": {
-                "items": {
+        self.ind = es.client.IndicesClient(self.cli)
+        self.ret = None
+        try:
+            self.ret = self.ind.create(self.name, body={
+                "settings": {
+                    "number_of_shards": 1,
+                    "number_of_replicas": 0
+                },
+                "mappings": {
                     "properties": {
-                        "cost": {"type": "float"},
+                        "cost": {"type": "double"},
                         "count": {"type": "integer"},
                         "node": {"type": "binary"},
-                    }}}})
+                    }}})
+        except es.exceptions.RequestError as e:
+            print(f'HeapElastic: create index: Runtime error: {e}')
 
-    def __del__(self):
-        return 0
+    def delete(self):
+        try:
+            return self.ind.delete(self.name)
+        except es.exceptions.RequestError as e:
+            print(f'HeapElastic: create delete: Runtime error: {e}')
 
-    def heappush(self, item):
+    def heappush(self, cost: float, count: int, data):
         """Push item onto heap, maintaining the heap invariant."""
-        res = cli.index(index=self.name, id=1, body={'key': 'antonio'})
+        # The Base64 encoded binary value must not have embedded newlines \n.
+        item = {'cost': cost, 'count': count, 'node': base64.b64encode(pickle.dumps(data)).decode('UTF-8')}
+        return self.cli.index(self.name, body=item)
 
     def heappop(self):
         """Pop the smallest item off the heap, maintaining the heap invariant."""
-        res = cli.get(index=self.name, id=1)
-        return res
+        s = Search(using=self.cli, index=self.name).sort('cost')
+        s.aggs.metric("min_cost", "min", field="cost")
+        response = s.execute()
+        return response.aggregations.min_cost.value, response.hits[0].count, \
+               pickle.loads(base64.b64decode(response.hits[0].node.encode('UTF-8')), encoding='UTF-8')
+
+
+if __name__ == "__main__":
+    import random
+
+    h = HeapElastic()
+    # print(h.ret)
+    for i in range(30000):
+        res = h.heappush(cost=random.uniform(0, 1), count=i, data='fff')
+        print('heappush: ', i)
+    min_cost, count, node = h.heappop()
+    print(min_cost, count, node)
+    # print('Delete: ', h.delete())
